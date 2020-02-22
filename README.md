75a3cc2bd883326d87dbb8e455a9893d2fb0de5f
ca1halman <minertimer123@gmail.com>
2020-02-22 17:44:04 -0500

    Require linux src before configuring

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ## Building
 
+Make sure you have the typical build stuff (gcc & company), as well as flex and bison installed.
+
 Configure:
 ```bash
 ./configure.sh
--- configure.sh
+++ configure.sh
@@ -1,4 +1,13 @@
-cd linux
+LINUXDIR="./linux"
+
+if [ ! -d $LINUXDIR ]; then
+	echo Linux Dir not found, please run \'git submodule init\'
+	exit 1
+fi
+
+cd $DIR
+
+echo Configuring for arm64
 
 make distclean
 make ARCH=arm64 defconfig
